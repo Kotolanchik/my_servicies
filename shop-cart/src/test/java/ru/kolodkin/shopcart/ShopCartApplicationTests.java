f99a5860c949536b84d95c76ffc95fb46bcc9409
konstantin <kostya.kolodkin@inbox.ru>
2023-01-29 22:46:14 +0300

    заготовка для корзины

---
+++ shop-cart/src/test/java/ru/kolodkin/shopcart/ShopCartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kolodkin.shopcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
